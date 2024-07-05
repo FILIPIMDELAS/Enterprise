e88e88de59e98c792ebaef2f247ab351faa2dbd2
Felipe C. Valverde <felipecaetanodev@gmail.com>
2024-07-05 10:05:16 -0300

    First commit

--- Problema ao passar função/ContainerPhase/Container.jsx
+++ Problema ao passar função/ContainerPhase/Container.jsx
@@ -1,18 +1,45 @@
-import './Container.css'
-import ContainerContainer from '../ContainerContainer/ContainerContainer'
-import HeaderContainer from '../HeaderContainer/HeaderContainer'
+import './Container.css';
+import ContainerContainer from '../ContainerContainer/ContainerContainer';
+import HeaderContainer from '../HeaderContainer/HeaderContainer';
 import { useState } from 'react';
 
-function ContainerPhases(){
-    const [dadosCompartilhados, setDadosCompartilhados] = useState('');
-    const [funcaoCompartilhada, setFuncaoCompartilhada] = useState('');
+function ContainerPhases() {
+  const [dadosCompartilhados, setDadosCompartilhados] = useState();
 
-    return (
-        <div className='Container' id="ContainerPhase">
-            <HeaderContainer setDadosCompartilhados={setDadosCompartilhados} funcao={funcaoCompartilhada}/>
-            <ContainerContainer dadosCompartilhados={dadosCompartilhados} funcao={setFuncaoCompartilhada}/>
-        </div>
-    )
+  const updateChartData = () => {
+    const newData = {
+      labels: ['teste', 'kabum', 'lalala', 'podecre', 'tututu'],
+      datasets: [
+        {
+          label: 'Etapas',
+          data: [
+            calcularSomaValores('teste'),
+            calcularSomaValores('kabum'),
+            calcularSomaValores('lalala'),
+            calcularSomaValores('podecre'),
+            calcularSomaValores('tututu'),
+          ],
+          backgroundColor: 'rgba(54, 162, 235, 0.5)',
+          borderColor: 'rgba(54, 162, 235, 0.5)',
+          borderWidth: 1,
+        },
+      ],
+    };
+    setData(newData);
+  };
+
+  return (
+    <div className='Container' id='ContainerPhase'>
+      <HeaderContainer
+        setDadosCompartilhados={setDadosCompartilhados}
+        funcao={updateChartData()}
+      />
+      <ContainerContainer
+        dadosCompartilhados={dadosCompartilhados}
+        funcao={updateChartData()}
+      />
+    </div>
+  );
 }
 
-export default ContainerPhases
+export default ContainerPhases;
